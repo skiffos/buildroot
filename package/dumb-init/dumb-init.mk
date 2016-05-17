f0fb0e56b11f3fe075962d0360e1001a83a2d613
Christian Stewart <christian@paral.in>
2016-05-17 14:10:00 -0700

    dumb-init: new package
    dumb-init is a minimal init process to act as PID 1 for containers.
    
    Static compilation is used, as dumb-init will typically be executed inside a
    container.
    
    dumb-init can satisfy docker-engine's docker-init requirement.
    
    Signed-off-by: Christian Stewart <christian@paral.in>
    
    ---
    
    Changes since v1:
    
     - Upgraded to v1.2.1
     - Added license hash

---
+++ package/dumb-init/dumb-init.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# dumb-init
+#
+################################################################################
+
+DUMB_INIT_VERSION = v1.2.1
+DUMB_INIT_SITE = $(call github,Yelp,dumb-init,$(DUMB_INIT_VERSION))
+
+DUMB_INIT_LICENSE = MIT
+DUMB_INIT_LICENSE_FILES = LICENSE
+DUMB_INIT_CFLAGS += -static
+
+define DUMB_INIT_BUILD_CMDS
+	$(TARGET_CC) \
+		$(DUMB_INIT_CFLAGS) \
+		-o $(@D)/dumb-init $(@D)/dumb-init.c
+endef
+
+define DUMB_INIT_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/dumb-init $(TARGET_DIR)/usr/bin/dumb-init
+endef
+
+$(eval $(generic-package))
