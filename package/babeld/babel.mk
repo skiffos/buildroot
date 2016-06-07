ad63a4196e29946c49217073e2f9d6dfbcd8e875
Christian Stewart <christian@paral.in>
2016-06-06 21:42:40 -0700

    babeld: new package
    Babel is a loop-avoiding distance-vector routing protocol for IPv6 and
    IPv4 with fast convergence properties. It is based on the ideas in DSDV,
    AODV and Cisco's EIGRP, but is designed to work well not only in wired
    networks but also in wireless mesh networks, and has been extended with
    support for overlay networks.
    
    https://www.irif.univ-paris-diderot.fr/~jch/software/babel/
    
    Signed-off-by: Christian Stewart <christian@paral.in>

---
+++ package/babeld/babel.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# babeld
+#
+################################################################################
+
+BABELD_VERSION = 1.7.1
+BABELD_SITE = https://www.irif.univ-paris-diderot.fr/~jch/software/files
+BABELD_SOURCE = babeld-$(BABELD_VERSION).tar.gz
+BABELD_LICENSE = as-is
+BABELD_LICENSE_FILES = LICENSE
+
+define BABELD_BUILD_CMDS
+	$(MAKE) -C $(@D)
+endef
+
+define BABELD_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 755 $(@D)/babeld $(TARGET_DIR)/usr/bin/babeld
+endef
+
+$(eval $(generic-package))
