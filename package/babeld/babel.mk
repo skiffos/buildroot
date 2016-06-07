83d5449b8d9fd5e87fc677d7a9d2db179ac96f27
Christian Stewart <christian@paral.in>
2016-06-06 21:42:40 -0700

    babeld: new package
    Babel is a loop-avoiding distance-vector routing protocol for IPv6 and
    IPv4 with fast convergence properties. It is based on the ideas in DSDV,
    AODV and Cisco's EIGRP, but is designed to work well not only in wired
    networks but also in wireless mesh networks, and has been extended with
    support for overlay networks.
    
    https://www.irif.univ-paris-diderot.fr/~jch/software/babel/
    
    Signed-off-by: Christian Stewart <christian@paral.in>
    
    ---
    
    Changes for v2:
     - fix MAKE to actually cross-compile to target
     - use commit message as the help message
     - added note about verifying signature
    
    Changes for v3:
     - use TARGET_CC, not HOST_CC, d'oh
     - extensive runtime testing on ARM
     - as-is is MIT

---
+++ package/babeld/babel.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# babeld
+#
+################################################################################
+
+BABELD_VERSION = 1.7.1
+BABELD_SITE = https://www.irif.univ-paris-diderot.fr/~jch/software/files
+BABELD_SOURCE = babeld-$(BABELD_VERSION).tar.gz
+BABELD_LICENSE = MIT
+BABELD_LICENSE_FILES = LICENSE
+
+define BABELD_BUILD_CMDS
+	$(MAKE) -C $(@D) CC="$(TARGET_CC)" CFLAGS="$(TARGET_CFLAGS)" LDFLAGS="$(TARGET_LDFLAGS)"
+endef
+
+define BABELD_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 755 $(@D)/babeld $(TARGET_DIR)/usr/bin/babeld
+endef
+
+$(eval $(generic-package))
