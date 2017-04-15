77571f0ed83ca1aa5ec34202eeec389c43e8fa05
Christian Stewart <christian@paral.in>
2017-04-15 13:09:32 -0400

    rtl8821au: bump to latest
    Two CFLAGS are added: -Wno-unused-function and -Wno-unused-variable.
    
    These warnings are disabled due to several unused functions and
    variables in the codebase that cause build failures with the
    -Wall -Werror flags enabled in rtl8821au.
    
    Signed-off-by: Christian Stewart <christian@paral.in>

--- package/rtl8821au/rtl8821au.mk
+++ package/rtl8821au/rtl8821au.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RTL8821AU_VERSION = c33ddb05a77741d2a9c9b974ad0cf0fa26d17b6e
+RTL8821AU_VERSION = 0292aa694d85e8f9120479604e98693f20d5da02
 RTL8821AU_SITE = $(call github,abperiasamy,rtl8812AU_8821AU_linux,$(RTL8821AU_VERSION))
 RTL8821AU_LICENSE = GPLv2
 RTL8821AU_LICENSE_FILES = COPYING
@@ -12,7 +12,8 @@ RTL8821AU_LICENSE_FILES = COPYING
 RTL8821AU_MODULE_MAKE_OPTS = \
 	CONFIG_RTL8812AU_8821AU=m \
 	KVER=$(LINUX_VERSION_PROBED) \
-	USER_EXTRA_CFLAGS=-DCONFIG_$(call qstrip,$(BR2_ENDIAN))_ENDIAN
+	USER_EXTRA_CFLAGS="-DCONFIG_$(call qstrip,$(BR2_ENDIAN))_ENDIAN \
+		-Wno-unused-variable -Wno-unused-function"
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
