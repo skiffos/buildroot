79ec9cd5c6bd3431ac62c9c8c5f800ac80b0af7f
Christian Stewart <christian@paral.in>
2017-05-11 03:48:56 -0400

    dumb-init: add option to statically compile
    In many cases docker-init backing processes like dumb-init must be static, as
    they are executed inside a container in an unknown environment. This
    patch adds an option to specifically compile dumb-init statically, even if
    the system is compiled dynamically.
    
    Signed-off-by: Christian Stewart <christian@paral.in>

--- package/dumb-init/dumb-init.mk
+++ package/dumb-init/dumb-init.mk
@@ -10,8 +10,13 @@ DUMB_INIT_SOURCE = $(DUMB_INIT_VERSION).tar.gz
 DUMB_INIT_LICENSE = MIT
 DUMB_INIT_LICENSE_FILES = LICENSE
 
+ifeq ($(BR2_PACKAGE_DUMB_INIT_STATIC),y)
+DUMB_INIT_CFLAGS += -static
+endif
+
 define DUMB_INIT_BUILD_CMDS
 	$(TARGET_CC) \
+		$(DUMB_INIT_CFLAGS) \
 		-o $(@D)/dumb-init $(@D)/dumb-init.c
 endef
 
