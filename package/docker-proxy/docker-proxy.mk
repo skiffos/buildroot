0f005a2587d01873c55e9f174f452569a9c86a0f
Christian Stewart <christian@paral.in>
2020-01-09 20:18:41 -0800

    package/docker-proxy: bump to latest commit hash a543cbc
    docker-proxy (libnetwork) has not added a release tag for a long time. Use the
    latest master commit hash from 09 Dec 2020 instead.
    
    Signed-off-by: Christian Stewart <christian@paral.in>

--- package/docker-proxy/docker-proxy.mk
+++ package/docker-proxy/docker-proxy.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DOCKER_PROXY_VERSION = 55685ba49593e67f5e1c8180539379b16736c25e
+DOCKER_PROXY_VERSION = a543cbc4871f904b0efe205708eb45d72e65fd8b
 DOCKER_PROXY_SITE = $(call github,docker,libnetwork,$(DOCKER_PROXY_VERSION))
 
 DOCKER_PROXY_LICENSE = Apache-2.0
