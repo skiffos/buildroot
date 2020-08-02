2e0fb7f1a1ee1147f395939a6e51cc014627732b
Christian Stewart <christian@paral.in>
2020-08-02 13:48:05 -0700

    package/rpi-firmware: bump to kernel 5.10.0
    Signed-off-by: Christian Stewart <christian@paral.in>

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 358f4c271f2bc10931aae8fe0879ffaded8b9c44
+RPI_FIRMWARE_VERSION = 8a5549c137dbce0e082ecf21607dc5e3feac1160
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
