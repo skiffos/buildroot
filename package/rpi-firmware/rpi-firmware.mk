2a9cb1209ab27ea16a21e8d87ceb19fe249e7e19
Christian Stewart <christian@paral.in>
2020-08-02 13:48:05 -0700

    package/rpi-firmware: bump to latest working rev
    Signed-off-by: Christian Stewart <christian@paral.in>

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 5574077183389cd4c65077ba18b59144ed6ccd6d
+RPI_FIRMWARE_VERSION = 8174b21b65f26e6b71aca63b6c4686ff583cccca
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
