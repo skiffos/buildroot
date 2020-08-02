8a510c9cb3a14c72115576fd4be592c7797f9e7e
Christian Stewart <christian@paral.in>
2020-08-02 13:48:05 -0700

    package/rpi-firmware: bump to 5.10.52-r2
    For a full changelog, please see the repository:
    
    https://github.com/raspberrypi/firmware
    
    Signed-off-by: Christian Stewart <christian@paral.in>

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 1eb8921ae7158249894f89e8bb6c4bfb04ec7377
+RPI_FIRMWARE_VERSION = e83f41345103a7fec73ccc5a2970d1812de77cdf
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
