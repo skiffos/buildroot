3ea8e41c75d9be06b42d1fdd23738fa0e4e97c7e
Christian Stewart <christian@aperture.us>
2020-08-23 00:00:30 -0700

    package/nvidia-driver: bump to version 510.73.05
    This series is currently marked "stable" on the gentoo repo.
    
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- package/nvidia-driver/nvidia-driver.mk
+++ package/nvidia-driver/nvidia-driver.mk
@@ -4,8 +4,15 @@
 #
 ################################################################################
 
+# current stable (per gentoo)
+NVIDIA_DRIVER_VERSION = 510.73.05
+
+ifeq ($(BR2_x86_64),y)
+NVIDIA_DRIVER_SUFFIX = _64
+else
 NVIDIA_DRIVER_VERSION = 390.151
-NVIDIA_DRIVER_SUFFIX = $(if $(BR2_x86_64),_64)
+endif # BR2_x86_64
+
 NVIDIA_DRIVER_SITE = http://download.nvidia.com/XFree86/Linux-x86$(NVIDIA_DRIVER_SUFFIX)/$(NVIDIA_DRIVER_VERSION)
 NVIDIA_DRIVER_SOURCE = NVIDIA-Linux-x86$(NVIDIA_DRIVER_SUFFIX)-$(NVIDIA_DRIVER_VERSION)$(if $(BR2_x86_64),-no-compat32).run
 NVIDIA_DRIVER_LICENSE = NVIDIA Software License
