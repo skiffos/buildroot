a365664bd1fc6c25fe1acf756e25b48e98c83b47
Christian Stewart <christian@paral.in>
2020-12-05 00:17:34 -0800

    package/rpi-wifi-firmware: bump version to c13705
    Sync with RPi-Distro repos, add Pi 400 firmware.
    
    Signed-off-by: Christian Stewart <christian@paral.in>

--- package/rpi-wifi-firmware/rpi-wifi-firmware.mk
+++ package/rpi-wifi-firmware/rpi-wifi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_WIFI_FIRMWARE_VERSION = 688531da4bcf802a814d9cb0c8b6d62e3b8a3327
+RPI_WIFI_FIRMWARE_VERSION = c13705adcee0b35824db70b5f035c86a7088dfce
 RPI_WIFI_FIRMWARE_SITE = $(call github,LibreELEC,brcmfmac_sdio-firmware-rpi,$(RPI_WIFI_FIRMWARE_VERSION))
 RPI_WIFI_FIRMWARE_LICENSE = PROPRIETARY
 RPI_WIFI_FIRMWARE_LICENSE_FILES = LICENCE.broadcom_bcm43xx
