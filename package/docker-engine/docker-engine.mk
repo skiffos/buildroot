c47c9f6456d09b0ef1ee1c7aea523d06f4e1b56e
Christian Stewart <christian@paral.in>
2020-12-09 22:25:54 -0800

    package/docker-engine: bump version to 20.10.2
    Signed-off-by: Christian Stewart <christian@paral.in>

--- package/docker-engine/docker-engine.mk
+++ package/docker-engine/docker-engine.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DOCKER_ENGINE_VERSION = 20.10.1
+DOCKER_ENGINE_VERSION = 20.10.2
 DOCKER_ENGINE_SITE = $(call github,moby,moby,v$(DOCKER_ENGINE_VERSION))
 
 DOCKER_ENGINE_LICENSE = Apache-2.0
@@ -13,13 +13,6 @@ DOCKER_ENGINE_LICENSE_FILES = LICENSE
 DOCKER_ENGINE_DEPENDENCIES = host-pkgconf
 DOCKER_ENGINE_GOMOD = github.com/docker/docker
 
-DOCKER_ENGINE_LDFLAGS = \
-	-X $(DOCKER_ENGINE_GOMOD)/dockerversion.BuildTime="" \
-	-X $(DOCKER_ENGINE_GOMOD)/dockerversion.GitCommit="buildroot" \
-	-X $(DOCKER_ENGINE_GOMOD)/dockerversion.IAmStatic="false" \
-	-X $(DOCKER_ENGINE_GOMOD)/dockerversion.InitCommitID="" \
-	-X $(DOCKER_ENGINE_GOMOD)/dockerversion.Version="$(DOCKER_ENGINE_VERSION)"
-
 DOCKER_ENGINE_TAGS = cgo exclude_graphdriver_zfs
 DOCKER_ENGINE_BUILD_TARGETS = cmd/dockerd
 
@@ -27,6 +20,13 @@ ifeq ($(BR2_PACKAGE_LIBAPPARMOR),y)
 DOCKER_ENGINE_DEPENDENCIES += libapparmor
 DOCKER_ENGINE_TAGS += apparmor
 endif
+
+DOCKER_ENGINE_LDFLAGS = \
+	-X $(DOCKER_ENGINE_GOMOD)/dockerversion.BuildTime="" \
+	-X $(DOCKER_ENGINE_GOMOD)/dockerversion.GitCommit="buildroot" \
+	-X $(DOCKER_ENGINE_GOMOD)/dockerversion.IAmStatic="false" \
+	-X $(DOCKER_ENGINE_GOMOD)/dockerversion.InitCommitID="" \
+	-X $(DOCKER_ENGINE_GOMOD)/dockerversion.Version="$(DOCKER_ENGINE_VERSION)"
 
 ifeq ($(BR2_PACKAGE_LIBSECCOMP),y)
 DOCKER_ENGINE_TAGS += seccomp
