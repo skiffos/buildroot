aa3ed87a40adab0eb6e156caac5ea0eb0b967dcf
Christian Stewart <christian@paral.in>
2021-05-02 19:13:38 -0700

    package/fuse-overlayfs: enable building as host package
    Signed-off-by: Christian Stewart <christian@paral.in>
    
    ---
    
    v2 -> v3:
    
     - fixed newline at end of config.in.host file

--- package/fuse-overlayfs/fuse-overlayfs.mk
+++ package/fuse-overlayfs/fuse-overlayfs.mk
@@ -12,4 +12,8 @@ FUSE_OVERLAYFS_LICENSE_FILES = COPYING
 FUSE_OVERLAYFS_AUTORECONF = YES
 FUSE_OVERLAYFS_DEPENDENCIES = libfuse3 host-pkgconf
 
+HOST_FUSE_OVERLAYFS_AUTORECONF = YES
+HOST_FUSE_OVERLAYFS_DEPENDENCIES = host-libfuse3 host-pkgconf
+
 $(eval $(autotools-package))
+$(eval $(host-autotools-package))
