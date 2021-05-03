d3b34cb9b109f0fd033eb2862d397c64f8770c18
Christian Stewart <christian@aperture.us>
2021-05-02 19:19:27 -0700

    package/genuinetools-img: new host and target package
    Img is a standalone, daemon-less, unprivileged Dockerfile and OCI compatible
    container image builder. It uses the moby-buildkit solver internally. The
    commands/UX mirror the Docker CLI for workflow compatibility.
    
    Using the genuinetools- prefix due to the generic nature of the "img" name.
    
    Adds both host and target variants. fuse-overlayfs can be used as an
    unprivileged and rootless overlay filesystem backend.
    
    https://github.com/genuinetools/img
    
    Signed-off-by: Christian Stewart <christian@aperture.us>
    
    ---
    
    TODO:
    
    https://patchwork.ozlabs.org/project/buildroot/patch/20210516010521.1375959-1-christian@aperture.us/
    
     - changes to patch to add libfuse3 as host package requested.

---
+++ package/genuinetools-img/genuinetools-img.mk
@@ -0,0 +1,26 @@
+################################################################################
+#
+# genuinetools-img
+#
+################################################################################
+
+GENUINETOOLS_IMG_VERSION = 0.5.11
+GENUINETOOLS_IMG_SITE = $(call github,genuinetools,img,v$(GENUINETOOLS_IMG_VERSION))
+
+GENUINETOOLS_IMG_LICENSE = MIT
+GENUINETOOLS_IMG_LICENSE_FILES = LICENSE
+
+GENUINETOOLS_IMG_DEPENDENCIES = host-pkgconf
+GENUINETOOLS_IMG_GOMOD = github.com/genuinetools/img
+
+GENUINETOOLS_IMG_TAGS = cgo noembed
+GENUINETOOLS_IMG_LDFLAGS = \
+	-X $(GENUINETOOLS_IMG_GOMOD)/version.VERSION="$(GENUINETOOLS_IMG_VERSION)"
+
+HOST_GENUINETOOLS_IMG_TAGS = $(GENUINETOOLS_IMG_TAGS)
+HOST_GENUINETOOLS_IMG_LDFLAGS = $(GENUINETOOLS_IMG_LDFLAGS)
+
+HOST_GENUINETOOLS_IMG_BIN_NAME = genuinetools-img
+
+$(eval $(golang-package))
+$(eval $(host-golang-package))
