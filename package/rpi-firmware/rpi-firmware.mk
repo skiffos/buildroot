fd840d263bdf1db59412abf67cb04af223706ce9
Christian Stewart <christian@paral.in>
2021-05-21 15:55:44 -0700

    package/rpi-firmware: bump to kernel 5.10.79
    For a full changelog, please see the repository:
    
    https://github.com/raspberrypi/firmware
    
    Signed-off-by: Christian Stewart <christian@paral.in>

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 3f20b832b27cd730deb6419b570f31a98167eef6
+RPI_FIRMWARE_VERSION = 12bc6e3677348adaffd155e7a04761e2661d4bff
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
