fbe3b858c4ab195aec49a465341907ce28cbb318
Christian Stewart <christian@paral.in>
2021-05-21 15:55:44 -0700

    package/rpi-firmware: switch to stable branch 1.20210527
    Signed-off-by: Christian Stewart <christian@paral.in>

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = d016a6eb01c8c7326a89cb42809fed2a21525de5
+RPI_FIRMWARE_VERSION = 1eb8921ae7158249894f89e8bb6c4bfb04ec7377
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
