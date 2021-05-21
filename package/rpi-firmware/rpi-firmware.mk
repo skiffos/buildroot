6c266244e1b671d5f64ee464f8514c068f48563b
Christian Stewart <christian@aperture.us>
2021-05-21 15:55:44 -0700

    package/rpi-firmware: bump to latest
    For a full changelog, please see the repository:
    
    https://github.com/raspberrypi/firmware
    
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 3f20b832b27cd730deb6419b570f31a98167eef6
+RPI_FIRMWARE_VERSION = 0dc4dd2af498c8c06dd1f10b670a37b79bb4a9e8
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
