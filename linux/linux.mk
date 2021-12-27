ba013e571d5501b8bdbd0296d56d80ccd1e96f5a
Christian Stewart <christian@paral.in>
2021-12-26 16:28:08 -0800

    [RFC] linux: add needs-host-libyaml option
    Signed-off-by: Christian Stewart <christian@paral.in>

--- linux/linux.mk
+++ linux/linux.mk
@@ -115,6 +115,10 @@ ifeq ($(BR2_LINUX_KERNEL_NEEDS_HOST_LIBELF),y)
 LINUX_DEPENDENCIES += host-elfutils host-pkgconf
 endif
 
+ifeq ($(BR2_LINUX_KERNEL_NEEDS_HOST_LIBYAML),y)
+LINUX_DEPENDENCIES += host-libyaml
+endif
+
 # If host-uboot-tools is selected by the user, assume it is needed to
 # create a custom image
 ifeq ($(BR2_PACKAGE_HOST_UBOOT_TOOLS),y)
