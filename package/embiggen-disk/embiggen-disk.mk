4cec17394346d1faf4abb39528576a28b93ce2af
Christian Stewart <christian@paral.in>
2022-01-06 14:18:32 -0800

    package/embiggen-disk: drop patch and bump to 9e7b2f
    PR merged:
    
    https://github.com/bradfitz/embiggen-disk/pull/13#event-5854783562
    
    The changes were to support /dev/vd and /dev/mmcblk devices.
    
    Signed-off-by: Christian Stewart <christian@paral.in>

--- package/embiggen-disk/embiggen-disk.mk
+++ package/embiggen-disk/embiggen-disk.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-EMBIGGEN_DISK_VERSION = c554fc1c93a4004ce0b6a3f69b0dabe0481a2308
+EMBIGGEN_DISK_VERSION = 9e7b2fc7b99c4dece41a805489a6ca377ce55a62 
 EMBIGGEN_DISK_SITE = $(call github,bradfitz,embiggen-disk,$(EMBIGGEN_DISK_VERSION))
 EMBIGGEN_DISK_LICENSE = Apache-2.0
 EMBIGGEN_DISK_LICENSE_FILES = LICENSE
