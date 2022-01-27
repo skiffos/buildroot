641fae546777ad60f9e6da6f4e97494620a61d37
Christian Stewart <christian@paral.in>
2022-01-26 19:56:31 -0800

    package/runc: add host package
    Adds support for building runc as a host package.
    
    The bin name and gomod have to be specified as the pkg-golang infrastructure
    assumes the bin name will be "host-runc" on default.
    
    Signed-off-by: Christian Stewart <christian@paral.in>

--- package/runc/runc.mk
+++ package/runc/runc.mk
@@ -10,6 +10,8 @@ RUNC_LICENSE = Apache-2.0, LGPL-2.1 (libseccomp)
 RUNC_LICENSE_FILES = LICENSE
 RUNC_CPE_ID_VENDOR = linuxfoundation
 
+RUNC_GOMOD = github.com/opencontainers/runc
+
 RUNC_LDFLAGS = -X main.version=$(RUNC_VERSION)
 RUNC_TAGS = cgo static_build
 
@@ -23,4 +25,10 @@ RUNC_TAGS += seccomp
 RUNC_DEPENDENCIES += libseccomp host-pkgconf
 endif
 
+HOST_RUNC_BIN_NAME = runc
+HOST_RUNC_LDFLAGS = $(RUNC_LDFLAGS)
+HOST_RUNC_TAGS = $(RUNC_TAGS)
+HOST_RUNC_INSTALL_BINS = $(HOST_RUNC_BIN_NAME)
+
 $(eval $(golang-package))
+$(eval $(host-golang-package))
