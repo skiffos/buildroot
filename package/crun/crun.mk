b8fdc37a929e24774d4f55ab84094cb0c3fd21c3
Christian Stewart <christian@paral.in>
2022-08-20 20:15:38 -0700

    [WIP] crun: new package
    Signed-off-by: Christian Stewart <christian@paral.in>

--- package/crun/crun.mk
+++ package/crun/crun.mk
@@ -4,13 +4,9 @@
 #
 ################################################################################
 
-# TODO:
-# - .version file needs to be created in advance
-# - libocispec: needs separate package probably
-
-CRUN_VERSION = 1.2
+CRUN_VERSION = 1.5
 CRUN_SITE = $(call github,containers,crun,$(CRUN_VERSION))
-CRUN_DEPENDENCIES = host-pkgconf yajl
+CRUN_DEPENDENCIES = host-pkgconf libocispec yajl
 CRUN_LICENSE = GPL-2.0+ (crun binary), LGPL-2.1+ (libcrun)
 CRUN_LICENSE_FILES = COPYING COPYING.libcrun
 
@@ -21,5 +17,12 @@ CRUN_DEPENDENCIES += libseccomp
 else
 CRUN_CONF_OPTS += --disable-seccomp
 endif
+
+CRUN_CONF_OPTS += --enable-embedded-yajl=no
+
+define CRUN_GEN_VERSION
+	echo "$(CRUN_VERSION)" > $(@D)/.version
+endef
+CRUN_PRE_BUILD_HOOKS += CRUN_GEN_VERSION
 
 $(eval $(autotools-package))
