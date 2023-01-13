7e48da3c3f29d61106f6dea1c79754a9f8002df6
Christian Stewart <christian@paral.in>
2023-01-12 19:34:54 -0800

    [SKIFF] package/glibc: update to latest version 2023-01-12
    Signed-off-by: Christian Stewart <christian@paral.in>

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -7,7 +7,7 @@
 # Generate version string using:
 #   git describe --match 'glibc-*' --abbrev=40 origin/release/MAJOR.MINOR/master | cut -d '-' -f 2-
 # When updating the version, please also update localedef
-GLIBC_VERSION = 2.36-81-g4f4d7a13edfd2fdc57c9d76e1fd6d017fb47550c
+GLIBC_VERSION = 2.36.9000-434-g7e31d166510ac4adbf53d5e8144c709a37dd8c7a
 # Upstream doesn't officially provide an https download link.
 # There is one (https://sourceware.org/git/glibc.git) but it's not reliable,
 # sometimes the connection times out. So use an unofficial github mirror.
