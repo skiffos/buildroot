320224fafec268e242869b85a1231d39e322e3af
Christian Stewart <christian@paral.in>
2023-02-02 05:00:34 -0800

    package/docker-cli: bump version to v23.0.0
    This update requires a workaround for the non-standard vendor/ structure in the
    docker-engine repository, which has an invalid vendor/modules.txt file.
    
    Removing the vendor/modules.txt file fixes the build error.
    
    Reference: https://github.com/moby/moby/issues/44618#issuecomment-1343565705
    
    The go download post-process script uses go mod init to create the go.mod file
    which specifies a language version of go1.16 on default. Use go mod edit to
    adjust the go.mod file to the required minimum version go1.19 for generic types
    used by docker-engine. (Fixes a build error).
    
    Signed-off-by: Christian Stewart <christian@paral.in>
    
    ---
    
    v2 -> v3:
    
     - update comments regarding adjusting go.mod version
     - use rm -f instead of checking for file existence

--- package/docker-cli/docker-cli.mk
+++ package/docker-cli/docker-cli.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DOCKER_CLI_VERSION = 20.10.22
+DOCKER_CLI_VERSION = 23.0.0
 DOCKER_CLI_SITE = $(call github,docker,cli,v$(DOCKER_CLI_VERSION))
 
 DOCKER_CLI_LICENSE = Apache-2.0
@@ -28,6 +28,16 @@ DOCKER_CLI_LDFLAGS += -extldflags '-static'
 DOCKER_CLI_TAGS += osusergo netgo
 DOCKER_CLI_GO_ENV = CGO_ENABLED=no
 endif
+
+# set the go.mod language version to go1.19
+# remove the conflicting vendor/modules.txt
+# https://github.com/moby/moby/issues/44618#issuecomment-1343565705
+define DOCKER_ENGINE_CONFIGURE_CMDS
+	cd $(@D); \
+		$(HOST_GO_HOST_ENV) $(DOCKER_ENGINE_GO_ENV) \
+		$(GO_BIN) mod edit -go=1.19 go.mod
+	rm -f $(@D)/vendor/modules.txt
+endef
 
 DOCKER_CLI_INSTALL_BINS = $(notdir $(DOCKER_CLI_BUILD_TARGETS))
 
