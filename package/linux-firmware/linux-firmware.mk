94a3afd960a8f809fb203042ffe816f5df3bf3eb
Christian Stewart <christian@aperture.us>
2023-02-21 19:07:29 -0800

    package/linux-firmware: add support for RTL8192E
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- package/linux-firmware/linux-firmware.mk
+++ package/linux-firmware/linux-firmware.mk
@@ -631,6 +631,13 @@ LINUX_FIRMWARE_FILES += \
 	rtl_nic/rtl8411-2.fw
 endif
 
+ifeq ($(BR2_PACKAGE_LINUX_FIRMWARE_RTL_8192E),y)
+LINUX_FIRMWARE_FILES += \
+	RTL8192E/boot.img \
+	RTL8192E/data.img \
+	RTL8192E/main.img
+endif
+
 ifeq ($(BR2_PACKAGE_LINUX_FIRMWARE_XCx000),y)
 LINUX_FIRMWARE_FILES += \
 	dvb-fe-xc4000-1.4.1.fw \
