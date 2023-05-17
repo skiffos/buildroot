7249808b9be9a13342b4d185e418965a112bfd15
Christian Stewart <christian@aperture.us>
2023-05-16 19:12:54 -0700

    linux: add support for building device tree overlays
    Some kernels have device tree overlay files:
    
    make arch/arm64/boot/dts/amlogic/overlays/odroidn2/i2c0.dtbo
    
    Add an option to add these dtbo files to the LINUX_DTBS list.
    
    BR2_LINUX_KERNEL_INTREE_DTS_OVERLAY_NAME="amlogic/overlays/odroidn2/i2c0"
    
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- linux/linux.mk
+++ linux/linux.mk
@@ -227,6 +227,11 @@ endif
 
 LINUX_DTBS = $(addsuffix .dtb,$(LINUX_DTS_NAME)) $(addsuffix .dtbo,$(LINUX_DTSO_NAMES))
 
+LINUX_DTS_OVERLAY_NAME += $(call qstrip,$(BR2_LINUX_KERNEL_INTREE_DTS_OVERLAY_NAME))
+LINUX_DTS_OVERLAY_NAME += $(basename $(filter %.dts,$(notdir $(call qstrip,$(BR2_LINUX_KERNEL_CUSTOM_DTS_OVERLAY_PATH)))))
+
+LINUX_DTBS += $(addsuffix .dtbo,$(LINUX_DTS_OVERLAY_NAME))
+
 ifeq ($(BR2_LINUX_KERNEL_IMAGE_TARGET_CUSTOM),y)
 LINUX_IMAGE_NAME = $(call qstrip,$(BR2_LINUX_KERNEL_IMAGE_NAME))
 LINUX_TARGET_NAME = $(call qstrip,$(BR2_LINUX_KERNEL_IMAGE_TARGET_NAME))
