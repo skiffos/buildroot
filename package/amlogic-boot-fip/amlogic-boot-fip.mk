202b2f195b764866f77270b20d819ec5293a244c
Christian Stewart <christian@aperture.us>
2023-10-02 12:20:33 -0700

    [SKIFF] package/amlogic-boot-fip: copy device directory to images
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- package/amlogic-boot-fip/amlogic-boot-fip.mk
+++ package/amlogic-boot-fip/amlogic-boot-fip.mk
@@ -11,22 +11,15 @@ AMLOGIC_BOOT_FIP_LICENSE_FILES = LICENSE
 AMLOGIC_BOOT_FIP_REDISTRIBUTE = NO
 
 AMLOGIC_BOOT_FIP_INSTALL_DIR = $(BINARIES_DIR)/amlogic-boot-fip
-
 AMLOGIC_BOOT_FIP_DEVICE = $(call qstrip,$(BR2_PACKAGE_HOST_AMLOGIC_BOOT_FIP_DEVICE))
 
-AMLOGIC_BOOT_FIP_FILES_TO_INSTALL = build-fip.sh axg.inc g12a.inc gxbb.inc gxl.inc
-
 define HOST_AMLOGIC_BOOT_FIP_INSTALL_CMDS
-	mkdir -p $(AMLOGIC_BOOT_FIP_INSTALL_DIR)/$(AMLOGIC_BOOT_FIP_DEVICE)
-	$(foreach f,$(AMLOGIC_BOOT_FIP_FILES_TO_INSTALL),\
-		$(INSTALL) -D -m 0755 $(@D)/$(f) $(AMLOGIC_BOOT_FIP_INSTALL_DIR)/$(f)
-	)
-	cp -rf $(@D)/$(AMLOGIC_BOOT_FIP_DEVICE) $(AMLOGIC_BOOT_FIP_INSTALL_DIR)
+	cp -dpfr $(@D)/$(AMLOGIC_BOOT_FIP_DEVICE)/. $(AMLOGIC_BOOT_FIP_INSTALL_DIR)/
 endef
 
 # check for empty device string when we're building
 ifeq ($(BR2_PACKAGE_HOST_AMLOGIC_BOOT_FIP)$(BR_BUILDING),yy)
-ifeq ($(call qstrip,$(BR2_PACKAGE_HOST_AMLOGIC_BOOT_FIP_DEVICE)),)
+ifeq ($(AMLOGIC_BOOT_FIP_DEVICE),)
 $(error No device specified for amlogic-boot-fip, please check your BR2_PACKAGE_HOST_AMLOGIC_BOOT_FIP_DEVICE setting)
 endif
 endif
