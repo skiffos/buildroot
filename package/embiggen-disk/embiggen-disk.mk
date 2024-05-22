01341289b4768e5f66871afbe77b95108654b331
Christian Stewart <christian@aperture.us>
2024-05-22 04:08:05 -0700

    package/embiggen-disk: bump version to v20240521
    Update to latest and update the tarball hash.
    
    Drop the GOMOD line since this is now the same as go.mod.
    
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- package/embiggen-disk/embiggen-disk.mk
+++ package/embiggen-disk/embiggen-disk.mk
@@ -4,11 +4,9 @@
 #
 ################################################################################
 
-EMBIGGEN_DISK_VERSION = v20220519
+EMBIGGEN_DISK_VERSION = v20240521
 EMBIGGEN_DISK_SITE = $(call github,skiffos,embiggen-disk,$(EMBIGGEN_DISK_VERSION))
 EMBIGGEN_DISK_LICENSE = Apache-2.0
 EMBIGGEN_DISK_LICENSE_FILES = LICENSE
-
-EMBIGGEN_DISK_GOMOD = github.com/bradfitz/embiggen-disk
 
 $(eval $(golang-package))
