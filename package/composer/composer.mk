skiffos/buildroot

commit ef57da23b9950a8820c64c268672265f233cfb00

package/composer: needs host-php to extract phar files
Although composer has host-php as build dependency, we use php in the
extract step, so it needs to be listed as an extract dependency,
otherwise host-php might not been built/installed before the extract
step of host-composer.

Fixes:

  http://autobuild.buildroot.net/results/50658c50fde8145fac320e3b17004e98c78c6c4d/

Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/composer/composer.mk
@@ -15,6 +15,7 @@ COMPOSER_LICENSE = MIT
 COMPOSER_LICENSE_FILES = LICENSE
 
 HOST_COMPOSER_DEPENDENCIES = host-php
+HOST_COMPOSER_EXTRACT_DEPENDENCIES = host-php
 
 define HOST_COMPOSER_EXTRACT_CMDS
 	cp $(HOST_COMPOSER_DL_DIR)/$(COMPOSER_SOURCE) $(@D)

commit a513b7b673aef2bd46655da797d185f0df7a9ed0

package/composer: fix improperly scoped variable
Fixes: ef57da23b9950a8820c64c268672265f233cfb00

Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

package/composer/composer.mk
@@ -9,8 +9,8 @@ COMPOSER_SOURCE = composer-$(COMPOSER_VERSION).phar
 # Here, we pass a dummy URL parameter in order to control the name the file
 # will have once downloaded.
 # Otherwise, the names will clash ifever we update the version.
-BASE_SITE = https://getcomposer.org/download/$(COMPOSER_VERSION)/composer.phar
-COMPOSER_SITE = $(BASE_SITE)?n=f/$(COMPOSER_SOURCE)
+COMPOSER_BASE_SITE = https://getcomposer.org/download/$(COMPOSER_VERSION)/composer.phar
+COMPOSER_SITE = $(COMPOSER_BASE_SITE)?n=f/$(COMPOSER_SOURCE)
 COMPOSER_LICENSE = MIT
 COMPOSER_LICENSE_FILES = LICENSE
 COMPOSER_CPE_ID_VENDOR = getcomposer