skiffos/buildroot

commit 4ac2cc5beec0eac4991fb28f286707a5659fa67e

package/openssh: security bump to version 9.8p1
Fixes CVE-2024-6387: Unauthenticated root login because of signal
handler race condition.

Drop upstream patch and autoreconf.

Signed-off-by: Baruch Siach <baruch@tkos.co.il>
Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

package/openssh/openssh.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENSSH_VERSION_MAJOR = 9.7
+OPENSSH_VERSION_MAJOR = 9.8
 OPENSSH_VERSION_MINOR = p1
 OPENSSH_VERSION = $(OPENSSH_VERSION_MAJOR)$(OPENSSH_VERSION_MINOR)
 OPENSSH_CPE_ID_VERSION = $(OPENSSH_VERSION_MAJOR)
@@ -12,9 +12,6 @@ OPENSSH_CPE_ID_UPDATE = $(OPENSSH_VERSION_MINOR)
 OPENSSH_SITE = http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable
 OPENSSH_LICENSE = BSD-3-Clause, BSD-2-Clause, Public Domain
 OPENSSH_LICENSE_FILES = LICENCE
-
-# 0001-Improve-detection-of-fzero-call-used-regs-used.patch
-OPENSSH_AUTORECONF = YES
 
 OPENSSH_CONF_ENV = \
 	LD="$(TARGET_CC)" \

package/openssh/0001-Improve-detection-of-fzero-call-used-regs-used.patch
@@ -1,32 +1,0 @@
-From f9193f03db0029fc9c31fbdb5c66a2737446bd8f Mon Sep 17 00:00:00 2001
-From: Darren Tucker <dtucker@dtucker.net>
-Date: Mon, 25 Mar 2024 09:28:02 +1100
-Subject: [PATCH] Improve detection of -fzero-call-used-regs=used.
-
-Should better detect problems with gcc 13 on m68k.  bz#3673 from Colin
-Watson via bz#3673 and https://gcc.gnu.org/bugzilla/show_bug.cgi?id=110934
-
-Signed-off-by: Darren Tucker <dtucker@dtucker.net>
-Upstream: https://github.com/openssh/openssh-portable/commit/f9193f03db0029fc9c31fbdb5c66a2737446bd8f
----
- m4/openssh.m4 | 3 +++
- 1 file changed, 3 insertions(+)
-
-diff --git a/m4/openssh.m4 b/m4/openssh.m4
-index 033df501c3d8..176a8d1c9282 100644
---- a/m4/openssh.m4
-+++ b/m4/openssh.m4
-@@ -20,7 +20,10 @@ char *f2(char *s, ...) {
- 	va_end(args);
- 	return strdup(ret);
- }
-+int i;
-+double d;
- const char *f3(int s) {
-+	i = (int)d;
- 	return s ? "good" : "gooder";
- }
- int main(int argc, char **argv) {
--- 
-2.43.0
-

package/openssh/openssh.hash
@@ -1,4 +1,4 @@
-# From https://www.openssh.com/txt/release-9.7
-sha256  490426f766d82a2763fcacd8d83ea3d70798750c7bd2aff2e57dc5660f773ffd  openssh-9.7p1.tar.gz
+# From https://www.openssh.com/txt/release-9.8
+sha256  dd8bd002a379b5d499dfb050dd1fa9af8029e80461f4bb6c523c49973f5a39f3  openssh-9.8p1.tar.gz
 # Locally calculated
 sha256  05c30446ba738934b3f1efa965b454c122ca26cc4b268e5ae6843f58ccd1b16d  LICENCE

commit 52ae660123ce38e19eb60b44869ded1cf7ae74ae

packages/openssh: install sshd-session
In OpenSSH 9.8 the server is split into two processes: sshd and
sshd-session which results in the following error, when trying
to start sshd:

Starting sshd: /usr/libexec/sshd-session does not exist or is not executable

This patch installs the sshd-session binary.

Fixes: https://gitlab.com/buildroot.org/buildroot/-/issues/11
Fixes: 4ac2cc5beec0eac4991fb28f286707a5659fa67e

Signed-off-by: Niklas Yann Wettengel <niyawe@niyawe.de>
Reviewed-by: Nevo Hed <nhed+buildroot@starry.com>
Tested-by: Fiona Klute (WIWA) <fiona.klute@gmx.de>
Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

package/openssh/openssh.mk
@@ -117,6 +117,7 @@ endif
 ifeq ($(BR2_PACKAGE_OPENSSH_SERVER),y)
 define OPENSSH_INSTALL_SERVER_PROGRAMS
 	$(INSTALL) -D -m 0755 $(@D)/sshd $(TARGET_DIR)/usr/sbin/sshd
+	$(INSTALL) -D -m 0755 $(@D)/sshd-session $(TARGET_DIR)/usr/libexec/sshd-session
 	$(INSTALL) -D -m 0755 $(@D)/sftp-server $(TARGET_DIR)/usr/libexec/sftp-server
 endef
 OPENSSH_POST_INSTALL_TARGET_HOOKS += OPENSSH_INSTALL_SERVER_PROGRAMS